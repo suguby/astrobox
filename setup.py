cbd66975fae5d97f5613b2f34eba2f6bbe77df3b
wad <suguby@gmail.com>
2018-09-04 14:06:47 +0300

    0.1.3 release

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='astrobox',
-    version='0.1.2',
+    version='0.1.3',
     packages=['astrobox', 'astrobox.themes.default'],
     include_package_data=True,
     license='BSD License',
