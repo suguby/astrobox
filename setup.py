773e0fc15aeb9ddffd47a9a425138be10b750ee0
waddik <suguby@gmail.com>
2020-06-04 15:04:38 +0300

    release 1.5.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('requirements.txt') as ff:
 
 setuptools.setup(
     name='astrobox',
-    version='1.5.0.dev1',
+    version='1.5.0',
     packages=setuptools.find_packages(),
     include_package_data=True,
     license='BSD License',
