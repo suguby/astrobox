fac5d862592a1cc490d27322ccb7bd44092cb5c8
Waddik <vadim.shandrinoff@moex.com>
2020-12-07 16:21:39 +0300

    установил MOTHERSHIP_HEALING_RATE = 0.5 для пробы

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('requirements.txt') as ff:
 
 setuptools.setup(
     name='astrobox',
-    version='1.7.0.dev1',
+    version='1.8.0.dev1',
     packages=setuptools.find_packages(),
     include_package_data=True,
     license='BSD License',
